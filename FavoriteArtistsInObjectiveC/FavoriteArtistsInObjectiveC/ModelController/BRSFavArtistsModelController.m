3bac8167d68ccb1e425a899010cea95e70e54d62
brysonS <brys1saclaus2@gmail.com>
2021-02-19 21:27:27 -0800

    Added a private interface and mutable (aka "var") array as an instance variable

--- FavoriteArtistsInObjectiveC/FavoriteArtistsInObjectiveC/ModelController/BRSFavArtistsModelController.m
+++ FavoriteArtistsInObjectiveC/FavoriteArtistsInObjectiveC/ModelController/BRSFavArtistsModelController.m
@@ -7,6 +7,14 @@
 
 #import "BRSFavArtistsModelController.h"
 
+//'private interface/private class extension' allows us to do a little extra that is hidden from the public interface
+@interface BRSFavArtistsModelController () {
+//instance variable that is the underlying data that properies use
+    NSMutableArray *_internalArtists;
+}
+
+@end
+
 @implementation BRSFavArtistsModelController
 
 @end
