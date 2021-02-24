104d233612d56f2071d856338d1abbd18b0b2fec
brysonS <brys1saclaus2@gmail.com>
2021-02-24 15:33:58 -0800

    Implemented persistence to controller.

--- FavoriteArtistsInObjectiveC/FavoriteArtistsInObjectiveC/ModelController/BRSFavArtistsModelController.m
+++ FavoriteArtistsInObjectiveC/FavoriteArtistsInObjectiveC/ModelController/BRSFavArtistsModelController.m
@@ -25,6 +25,16 @@
     if (self = [super init]) {
         _internalArtists = [[NSMutableArray alloc] init];
         
+        // LOAD
+        NSString *filePath = self.getFilePath;
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        if ([fileManager fileExistsAtPath:filePath]) {
+            NSDictionary *dictionary = [[NSDictionary alloc] initWithContentsOfURL:self.createFileURL];
+            [self updateArtistsArrayWithDictionary:dictionary];
+        }
+        
+        
     }
     return self;
 }
@@ -35,11 +45,55 @@
 - (NSArray<BRSArtist *> *)artistArray
 {
     return _internalArtists.copy;
-}
+}//
 
 - (void)addArtist:(BRSArtist *)artist;
 {
     [_internalArtists addObject:artist];
+    [self saveToPersistentStore];
+}//
+
+//MARK: - Persistence
+
+- (NSURL *)createFileURL
+{
+    NSString *fileName = @"FavoriteArtistData.plist";
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, true) firstObject];
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+    NSURL *fileURL = [baseURL URLByAppendingPathComponent:fileName];
+    return fileURL;
+}//
+
+- (NSString *)getFilePath
+{
+    return self.createFileURL.path;
+}//
+
+// SAVE
+-(void)saveToPersistentStore
+{
+    NSMutableDictionary *dictionary = [[NSMutableDictionary alloc] init];
+    for (BRSArtist *artist in _internalArtists) {
+        NSString *key = artist.artistName;
+        dictionary[key] = artist.toDictionary;
+    }
+    
+    [dictionary writeToURL:self.createFileURL atomically:YES];
 }
+
+- (void)updateArtistsArrayWithDictionary:(NSDictionary *)dictionary
+{
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    for (NSDictionary *artistDictionary in dictionary.allValues) {
+        BRSArtist *artist = [[BRSArtist alloc] initWithDictionary:artistDictionary];
+        [array addObject:artist];
+    }
+    
+    _internalArtists = array;
+}
+
+
+
+
 
 @end
